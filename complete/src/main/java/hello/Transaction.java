659f594b51caa73065b22b649f3551baaa73e387
Ayush Gupta <ayushgupta@localhost.localdomain>
2018-09-30 15:10:17 -0400

    testing

---
+++ complete/src/main/java/hello/Transaction.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class Transaction {
+
+
+    public Transaction() {
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
