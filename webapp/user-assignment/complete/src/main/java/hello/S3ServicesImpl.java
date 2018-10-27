05805d791329e631fea6867fd27029af3241a3ce
prasadkanade777 <kanade.p@northeastern.edu>
2018-10-27 11:20:56 -0400

    updated all

--- webapp/user-assignment/complete/src/main/java/hello/S3ServicesImpl.java
+++ webapp/user-assignment/complete/src/main/java/hello/S3ServicesImpl.java
@@ -91,11 +91,11 @@ public class S3ServicesImpl implements S3Services{
 
     public File convertFromMultipart(MultipartFile file) throws Exception
     {
-        File convFile = new File(file.getOriginalFilename());
-        /*if(!convFile.getParentFile().exists())
+        File convFile = new File("tmp/" + file.getOriginalFilename());
+        if(!convFile.getParentFile().exists())
             convFile.getParentFile().mkdir();
-        if(!convFile.exists())*/
-            convFile.createTempFile(file.getOriginalFilename(),null);
+        if(!convFile.exists())
+            convFile.createNewFile();
         //FileOutputStream fos = new FileOutputStream(convFile);
         //fos.write(file.getBytes());
         //fos.close();
