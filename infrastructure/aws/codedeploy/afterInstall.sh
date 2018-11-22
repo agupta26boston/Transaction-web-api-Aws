6f18e5b107c17847bd2577e13bed750abae60290
bengre.t <bengre.t@husky.neu.edu>
2018-11-21 22:07:00 -0500

    Corrected Typos

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -5,6 +5,7 @@ sudo systemctl stop tomcat.service
 sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
 
 sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+sudo /sbin/iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # cleanup log files
 sudo rm -rf /opt/tomcat/logs/catalina*
